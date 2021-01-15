c705773e7327a667d4a64f89f0b5b31146f1126d
Norman Huth <norman@huth.it>
2021-01-15 08:50:53 +0100

    Update Text

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ require_once '../Helpers/functions.php';
     <title>hallo_alexa_</title>
     <link href="<?= asset('css/app.css') ?>" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Hi! Mein Name ist Alexandra und ich bin seit einigen Wochen auf Twitch. Meine Community ist aufgeschlossen, freundlich und familiär!">
+    <meta name="description" content="Hi! Meine Name ist Alexa, komme aus Frankfurt am Main und streame seit dem 12.April.2020 auf Twitch. Meine Community ist aufgeschlossen, freundlich und familiär!">
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
